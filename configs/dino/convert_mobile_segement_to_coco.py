a75edf5c4f2f055fb6496254f783e266f25163f1
orlando23 <xiandao.airs@gmail.com>
2024-05-20 19:47:15 +0000

    orlando - for adding mmdetection for mobile segmentation

--- configs/dino/convert_mobile_segement_to_coco.py
+++ configs/dino/convert_mobile_segement_to_coco.py
@@ -9,18 +9,13 @@ from mmengine.utils import track_iter_progress
 
 
 def convert_mobile_segment_to_coco(ann_file, out_file, image_prefix):
-    raw_data_infos = load(ann_file)
-    data_infos = []
-    for k, v in raw_data_infos.items():
-        data_informat = copy(v)
-        data_informat['json_file'] = k
-        data_infos.append(data_informat)
+    data_infos = load(ann_file)
 
     annotations = []
     images = []
     obj_count = 0
-    for idx, v in enumerate(track_iter_progress(data_infos.values())):
-        filename = v['image_path']
+    for idx, (k, v) in enumerate(track_iter_progress(list(data_infos.items()))):
+        filename = k.replace(".json", ".jpeg")[1:]
         img_path = osp.join(image_prefix, filename)
         height, width = mmcv.imread(img_path).shape[:2]
 
@@ -28,38 +23,41 @@ def convert_mobile_segment_to_coco(ann_file, out_file, image_prefix):
         
         target_labels = ["small_bbox", "large_bbox"]
 
+        bbox = []
         for label in target_labels:
-            assert not v[label]
-            y, x, h, w, control_id = v[label]
-            poly = [(x, y), (x + w, y), (x + w, y + h), (x, y + h)]
-            poly = [p for x in poly for p in x]
-            data_anno = dict(
-                image_id=idx,
-                id=obj_count,
-                category_id=0,
-                bbox=[x, y, x + w, y + h],
-                area=h * w,
-                control_id=control_id,
-                segmentation=[poly],
-                iscrowd=0)
-            annotations.append(data_anno)
-            obj_count += 1
+            for x, y, w, h, control_id in v[label]:
+                if (x, y, w, h) not in bbox:
+                    bbox.append((x, y, w, h))
+                    poly = [(x, y), (x + w, y), (x + w, y + h), (x, y + h)]
+                    poly = [p for x in poly for p in x]
+                    data_anno = dict(
+                        image_id=idx,
+                        id=obj_count,
+                        category_id=0,
+                        bbox=[x, y, x + w, y + h],
+                        area=h * w,
+                        control_id=control_id,
+                        json_file=k,
+                        segmentation=[poly],
+                        iscrowd=0)
+                    annotations.append(data_anno)
+                    obj_count += 1
 
     coco_format_json = dict(
         images=images,
         annotations=annotations,
         categories=[{
             'id': 0,
-            'name': 'balloon'
+            'name': 'bbox'
         }])
-    dump(coco_format_json, out_file)
+    dump(coco_format_json, out_file, indent=1)
 
 def conf():
     args = argparse.ArgumentParser()
     args.add_argument("--train_ann_file", type=str, default="../../../../ScreenReaderData_train.json")
-    args.add_argument("--train_out_file", type=str, default="configs/dino/train/annotation_coco.json")
+    args.add_argument("--train_out_file", type=str, default="data/train/annotation_coco.json")
     args.add_argument("--val_ann_file", type=str, default="../../../../ScreenReaderData_val.json")
-    args.add_argument("--val_out_file", type=str, default="configs/dino/val/annotation_coco.json")
+    args.add_argument("--val_out_file", type=str, default="data/val/annotation_coco.json")
     args.add_argument("--image_prefix", type=str, default="../../../../")
     return args.parse_args()
 
